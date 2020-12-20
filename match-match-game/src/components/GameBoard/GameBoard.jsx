8ec519ead26a8e9fdf868f6e9976273f177ebe0d
Tatsiana Zhylevich <rocktania@mail.ru>
2020-12-20 23:04:33 +0300

    Add: GameBoard

---
+++ match-match-game/src/components/GameBoard/GameBoard.jsx
@@ -0,0 +1,12 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { Board } from '../components/Board'
+
+export function GameBoard() {
+    return(
+        <Board>
+            {/* <Timer /> */}
+            {/* <CardContainer></CardContainer> */}
+        </Board>
+    );
+}
