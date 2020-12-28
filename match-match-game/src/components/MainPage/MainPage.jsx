315d42c721fc0c4950dd3ac6014d161d5fb6a835
Tatsiana Zhylevich <rocktania@mail.ru>
2020-12-28 22:08:40 +0300

    add: audio

--- match-match-game/src/components/MainPage/MainPage.jsx
+++ match-match-game/src/components/MainPage/MainPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
@@ -7,28 +7,18 @@ import { logout } from '../actions';
 import { DifficultyLevel } from './DifficultyLevel';
 import { CardType } from './CardType';
 import { Button } from '../components/Button';
-
-import JingleBells from '../../sounds/JingleBells.mp3';
+import hoho from '../../sounds/hoho.mp3';
 
 export function MainPage() {
-  const [isChecked, setIsChecked] = useState(false);
-  const [playOn] = useSound(
-    JingleBells,
-    { volume: 0.25 },
-  );
-
-  const getMusic = () => {
-    if (isChecked) {
-      return '';
-    } return playOn;
-  };
+  const [play] = useSound(hoho);
 
   const history = useHistory();
   const dispatch = useDispatch();
 
   const goToGameBoard = useCallback(() => {
+    play();
     history.push('/GameBoard');
-  }, [history]);
+  }, [history, play]);
 
   const getReadRules = useCallback(() => {
     history.push('/Rules');
@@ -49,15 +39,6 @@ export function MainPage() {
       <Button onClick={getReadRules}> Read more </Button>
       <StyledText>...and now let's start!</StyledText>
       <ButtonGo type="submit" onClick={goToGameBoard}>GO!</ButtonGo>
-      <div>
-        <StyledText> Sound</StyledText>
-        <input
-          type="checkbox"
-          checked={isChecked}
-          size={30}
-          onChange={() => setIsChecked(getMusic())}
-        />
-      </div>
 
     </StyledMainPage>
 
